5a11f5176a2108742f67075ea0998ac6104b033f
Jeff Musa <jeffmusa@gmail.com>
2018-08-17 15:35:47 +0300

    gitsearch app

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { MainComponent } from './main/main.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
